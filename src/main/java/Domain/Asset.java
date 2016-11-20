ff44ec388ad95896063c9a1d437b97ef23908c63
dhanendras <dhanendra.chaudhary@gmail.com>
2016-11-21 02:55:53 +0530

    Add files via upload

---
+++ src/main/java/Domain/Asset.java
@@ -0,0 +1,20 @@
+package Domain;
+
+public class Asset {
+	
+	private String color;
+	private String carat;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getCarat() {
+		return carat;
+	}
+	public void setCarat(String carat) {
+		this.carat = carat;
+	}
+
+}
