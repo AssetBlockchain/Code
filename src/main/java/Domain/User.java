c74f7b9ee3f37deae6d064fdecfa65e80b5ff92a
dhanendras <dhanendra.chaudhary@gmail.com>
2016-11-22 03:10:08 +0530

    adding code

---
+++ src/main/java/Domain/User.java
@@ -0,0 +1,26 @@
+package Domain;
+
+public class User {
+
+	private String type;
+	private String identity;
+	private String password;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getIdentity() {
+		return identity;
+	}
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
